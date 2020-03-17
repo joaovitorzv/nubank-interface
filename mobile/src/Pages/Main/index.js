d3d96d5f88906c2c64297d64746075963c0a2e42
joao vitor <resetgamer1@gmail.com>
2020-03-17 20:37:51 -0300

    settings screen

--- mobile/src/Pages/Main/index.js
+++ mobile/src/Pages/Main/index.js
@@ -1,12 +1,47 @@
 import React from 'react';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
 
 import Header from '../../components/Header';
-import { Container } from './styles';
+import Tabs from '../../components/Tabs';
+import Menu from '../../components/Menu';
+
+import { 
+  Container, 
+  Content, 
+  Card, 
+  CardHeader, 
+  CardContent, 
+  CardFooter, 
+  Title,
+  Description, 
+  Annotation } from './styles';
 
 export default function Main() {
   return(
     <Container> 
       <Header />
+
+      <Content>
+        <Menu />
+        <Card>
+          <CardHeader>
+            <Icon name="attach-money" size={28} color="#666" />
+            <Icon name="visibility-off" size={28} color="#666" />
+          </CardHeader>
+          <CardContent>
+            <Title>Saldo Disponível</Title>
+            <Description>R$ 248,81</Description>
+          </CardContent>
+          <CardFooter>
+            <Annotation>
+              Transferência de R$ 20,00 recebida de Henrique Veras, Hoje às 18:51
+            </Annotation>
+          </CardFooter>
+        </Card>
+      </Content>
+      
+      <Tabs />
     </Container>
   );
 }
